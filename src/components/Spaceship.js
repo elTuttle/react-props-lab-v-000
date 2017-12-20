6b2418dc257386081f74c4d86d859f7074e8c959
John Tuttle <thejwt@gmail.com>
2017-12-20 18:39:49 +0000

    automatically backed up by learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -8,7 +8,7 @@ class Spaceship extends React.Component {
         <h1>{this.props.name}</h1>
         <h3>Speed: {this.props.speed}</h3>
         <h3>Has Rockets: {this.props.hasRockets}</h3>
-        <h3>Colors: {this.props.colors.join(", ")}
+        <h3>Colors: {this.props.colors.join(", ")}</h3>
       </div>
     )
   }
